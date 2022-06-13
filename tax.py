0c330e0556a85e744fd645b817e9f4ff44c1c91d
macwin84 <63136769+macwin84@users.noreply.github.com>
2022-06-12 20:20:29 -0400

    Add files via upload

---
+++ tax.py
@@ -0,0 +1,9 @@
+print("enter sales tax rate:")
+rate=float(input())
+print("enter price of item:")
+item=float(input())
+print("tax amount")
+print(item/rate)
+taxam=item/rate
+print("item total with tax")
+print(item-taxam)
