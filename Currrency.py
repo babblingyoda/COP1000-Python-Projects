4052778dc97d534e8a5197709fae7ad38756002e
macwin84 <63136769+macwin84@users.noreply.github.com>
2022-06-12 20:22:32 -0400

    Add files via upload

---
+++ Currrency.py
@@ -0,0 +1,8 @@
+print("enter amount of USD you want to exchange:")
+US = float(input())
+Euro = US/0.95
+Yen = US/129.34
+print("exchange amount for euro:")
+print(Euro)
+print("exhange amount for Yen:")
+print(Yen)
